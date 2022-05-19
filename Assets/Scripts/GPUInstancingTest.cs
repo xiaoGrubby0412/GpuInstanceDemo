12651a3c07239cb703d22d124913ad21328dd92c
wangman09 <wangman09@baidu.com>
2022-05-19 10:42:07 +0800

    commit project

---
+++ Assets/Scripts/GPUInstancingTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GPUInstancingTest : MonoBehaviour {
+
+	public Transform prefab;
+
+	public int instances = 5000;
+
+	public float radius = 50f;
+
+	void Start () {
+		for (int i = 0; i < instances; i++) {
+			Transform t = Instantiate(prefab);
+			t.localPosition = Random.insideUnitSphere * radius;
+			t.SetParent(transform);
+		}
+	}
+}
